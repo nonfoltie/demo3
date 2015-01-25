19cdceed278d58f1c3f388a0d9fa9b72d24c8204
nonfoltie <kone.manfereba@courrier.uqam.ca>
2015-01-25 12:01:00 -0500

    refactoring pour reduire le nombre de variable boolean et et ajout de la condition le mois ne doit pas etre superieur 12 ni inferieur a 1

--- src/projetinf2015h15/GestionAssurance.java
+++ src/projetinf2015h15/GestionAssurance.java
@@ -18,16 +18,13 @@ import net.sf.json.JSONSerializer;
  * @author Groupe 15
  */
 public class GestionAssurance {
-    
-    
+
     /**
      *
-     * @author Groupe 15
-     * Changement du fichier d'entrée.
-     * @return 
+     * @author Groupe 15 Changement du fichier d'entrée.
+     * @return
      * @throws java.io.IOException
-    */
-    
+     */
     public static final double TAUX_0_POUR_CENT = 0.00;
     public static final double TAUX_25_POUR_CENT = 0.25;
     public static final double TAUX_40_POUR_CENT = 0.4;
@@ -35,406 +32,399 @@ public class GestionAssurance {
     public static final double TAUX_70_POUR_CENT = 0.7;
     public static final double TAUX_90_POUR_CENT = 0.9;
     public static final double TAUX_100_POUR_CENT = 1.00;
-    
-     public static String chargerFichier() throws IOException{
-        
+
+    public static String chargerFichier() throws IOException {
+
         String texteJson = null;
-        
-        try{
-           texteJson = FileReader.loadFileIntoString("resources/inputFile.json", "UTF-8");
-        } catch (FileNotFoundException e){
+
+        try {
+            texteJson = FileReader.loadFileIntoString("resources/inputFile.json", "UTF-8");
+        } catch (FileNotFoundException e) {
             return texteJson;
-        } catch (Exception e){
+        } catch (Exception e) {
             return texteJson;
         }
-        
+
         return texteJson;
     }
+
     /**
      *
-     * @author Groupe 15
-     * Création de l'objet.
-     * @return 
+     * @author Groupe 15 Création de l'objet.
+     * @return
      * @throws java.io.IOException
-    */
-    public static JSONObject formaterObjet() throws IOException{
-        
+     */
+    public static JSONObject formaterObjet() throws IOException {
+
         String leTexte = chargerFichier();
         JSONObject objActuel = null;
-        
-        if (leTexte != null){
-            
+
+        if (leTexte != null) {
+
             objActuel = (JSONObject) JSONSerializer.toJSON(leTexte);
         }
         return objActuel;
-        
+
     }
+
     /**
      *
      * @author Groupe 15
      * @param objActuel
      * @return le numero du client.
-    */
-    public static String getNumeroClient(JSONObject objActuel){
-        
+     */
+    public static String getNumeroClient(JSONObject objActuel) {
+
         String numeroClient = null;
-        
-        if (objActuel != null){
+
+        if (objActuel != null) {
             numeroClient = objActuel.getString("client");
         }
         return numeroClient;
     }
+
     /**
      *
      * @author Groupe 15
      * @param objActuel
      * @return la catégorie du contrat.
-    */
+     */
     public static String getCategorieContrat(JSONObject objActuel) {
-        
+
         String leContrat = null;
-        
-        if (objActuel != null){
+
+        if (objActuel != null) {
             leContrat = objActuel.getString("contrat");
         }
         return leContrat;
     }
+
     /**
      *
      * @author Groupe 15
      * @param objActuel
      * @return le mois
-    */
-    public static String getMois(JSONObject objActuel){
+     */
+    public static String getMois(JSONObject objActuel) {
         String leMois = null;
-        
-        if (objActuel != null){
+
+        if (objActuel != null) {
             leMois = objActuel.getString("mois");
         }
         return leMois;
     }
+
     /**
      *
      * @author Groupe 15
      * @param objetJson
      * @return la liste des soins
-    */
-    public static List<JSONObject> listerLesReclamations(JSONObject objetJson ){
-        
+     */
+    public static List<JSONObject> listerLesReclamations(JSONObject objetJson) {
+
         List<JSONObject> listeReclamation = null;
-        
-        if (objetJson != null){
-            
+
+        if (objetJson != null) {
+
             listeReclamation = new ArrayList<>();
             String reclamation = objetJson.getString("reclamations");
             JSONArray tableauReclamation = (JSONArray) JSONSerializer.toJSON(reclamation);
-        
-            for (int i = 0; i < tableauReclamation.size(); i++){
-            
+
+            for (int i = 0; i < tableauReclamation.size(); i++) {
+
                 JSONObject objetCourant = tableauReclamation.getJSONObject(i);
                 listeReclamation.add(objetCourant);
             }
         }
         return listeReclamation;
     }
-     
+
     /**
      * @author Groupe 15
      * @param numero
      * @return vrai si le numéro du client est valide. Sinon, faux.
-    */
-    public static boolean validerNumeroClient(String numero){
-        
+     */
+    public static boolean validerNumeroClient(String numero) {
+
         boolean reponse = false;
         int i = 0;
-        int j ;
-        
-        if(numero != null && numero.trim().length() == 6){
-            
+        int j;
+
+        if (numero != null && numero.trim().length() == 6) {
+
             j = numero.trim().length();
-            
-            while(i < j && (numero.trim().charAt(i) >= '0' && numero.trim().charAt(i) <= '9')){
-               i++;
-            }  
+
+            while (i < j && (numero.trim().charAt(i) >= '0' && numero.trim().charAt(i) <= '9')) {
+                i++;
+            }
             reponse = i == j;
         }
-       return reponse; 
+        return reponse;
     }
-    
-   
+
     /**
-     * @author Groupe 15
-     * retourne la liste des dates des soins.
+     * @author Groupe 15 retourne la liste des dates des soins.
      * @param contrat
      * @return vrai si le contrat est valide. Sinon, faux.
-    */
-    public static boolean validerContrat(String contrat){
-        
+     */
+    public static boolean validerContrat(String contrat) {
+
         boolean reponse = false;
 
-        if(contrat != null && (contrat.equals("A")|| contrat.equals("B")
-                || contrat.equals("C") || contrat.equals("D"))){
-            
+        if (contrat != null && (contrat.equals("A") || contrat.equals("B")
+                || contrat.equals("C") || contrat.equals("D"))) {
+
             reponse = true;
-        }    
-         
-       return reponse;
+        }
+
+        return reponse;
     }
-    
+
     /**
      * @author Groupe 15
      * @param mois
      * @return vrai si la date est valide. Sinon, faux.
-    */
-     public static boolean validerFormatMois(String mois){
-      boolean formatEstValide = true;
-      boolean esNumeric =true;
-     if( mois== null ){
-         esNumeric = false;
-     }else {
-      String ch1 = mois.trim().substring(0,4);
-      String ch2 = mois.trim().substring(5);
-      
-             for(int i=0,j=0;i< ch1.length()&& j< ch2.length();i++,j++){
-              if(ch1.charAt(i)<'0'||ch1.charAt(i)>'9'||ch2.charAt(j)<'0'||ch2.charAt(j)>'9'){
-                  esNumeric = false;
-              }
-          }
-          if( mois.trim().length()!= 7 || mois.trim().substring(4,5).charAt(4)!='-'){
-             formatEstValide = false;    
-             
-          }
-      
-     }  
-     
-       return (esNumeric&&formatEstValide );
+     */
+    public static boolean validerFormatMois(String mois) {
+        boolean formatEstValide = false;
+
+        if (mois != null || mois.trim().length() == 7 || mois.trim().substring(4, 5).charAt(4) == '-') {
+            String ch1 = mois.trim().substring(0, 4);
+            String ch2 = mois.trim().substring(5);
+            for (int i = 0, j = 0; i < ch1.length() && j < ch2.length(); i++, j++) {
+                if (ch1.charAt(i) >= '0' && ch1.charAt(i) <= '9' && ch2.charAt(j) >= '0' && ch2.charAt(j) <= '9'
+                        && Integer.parseInt(ch2) >= 1 && Integer.parseInt(ch2) <= 12) {
+
+                    formatEstValide = true;
+
+                }
+            }
+
+        }
+
+        return formatEstValide;
     }
-    
+
     /**
      * @author Groupe 15
      * @param numSoin
      * @return vrai si le numéro du soin est valide. Sinon, faux.
-    */
-     public static boolean validerNumeroSoin(int numSoin){
-         
-          boolean reponse = false;
-        
-       if((numSoin == 0) || (numSoin == 100) || (numSoin == 200) || (numSoin == 400)
-               || (numSoin >= 300 && numSoin <= 399) || (numSoin == 500)
-               || (numSoin == 600) || (numSoin == 700)){
-          
-           reponse = true;
-       }
-       
-       return reponse;
-    }        
-     
-     /**
+     */
+    public static boolean validerNumeroSoin(int numSoin) {
+
+        boolean reponse = false;
+
+        if ((numSoin == 0) || (numSoin == 100) || (numSoin == 200) || (numSoin == 400)
+                || (numSoin >= 300 && numSoin <= 399) || (numSoin == 500)
+                || (numSoin == 600) || (numSoin == 700)) {
+
+            reponse = true;
+        }
+
+        return reponse;
+    }
+
+    /**
      *
-     * @author Groupe 15
-     * retourne la liste des dates des soins.
+     * @author Groupe 15 retourne la liste des dates des soins.
      * @param date
      * @return
-    */
-     public static boolean validerLaDate(String date){
-         
+     */
+    public static boolean validerLaDate(String date) {
+
         boolean reponse = false;
-        
-       if(date != null && date.length() >= 7){
-          
-           reponse = validerFormatMois(date.trim().substring(0,7));
-       }
-       
-       return reponse;
-    } 
-     
+
+        if (date != null && date.length() >= 7) {
+
+            reponse = validerFormatMois(date.trim().substring(0, 7));
+        }
+
+        return reponse;
+    }
+
     /**
      *
-     * @author Groupe 15
-     * retourne la liste des dates des soins.
+     * @author Groupe 15 retourne la liste des dates des soins.
      * @param montant
      * @return
-    */ 
-    public static boolean validerMontant(String montant){
-        
-        
-        return false; 
-    } 
+     */
+    public static boolean validerMontant(String montant) {
+
+        return false;
+    }
+
     /**
      *
-     * @author Groupe 15
-     * retourne le montant du remboursement.
+     * @author Groupe 15 retourne le montant du remboursement.
      * @param objet
      * @return
-    */
-    public static boolean validerLesSoins(JSONObject objet){
-        
-        
+     */
+    public static boolean validerLesSoins(JSONObject objet) {
+
         return false;
-    
-    } 
-    
+
+    }
+
     /**
      *
-     * @author Groupe 15
-     * retourne la liste des dates des soins.
+     * @author Groupe 15 retourne la liste des dates des soins.
      * @param rembourssement
-     * @param uneReclamation 
-    */
-    public static void modifierLeSoin(Double rembourssement,JSONObject uneReclamation){
-        
+     * @param uneReclamation
+     */
+    public static void modifierLeSoin(Double rembourssement, JSONObject uneReclamation) {
+
         String leMontant = rembourssement + "$";
         uneReclamation.discard("montant");
         uneReclamation.accumulate("montant", leMontant);
-        
+
     }
-    
+
     /**
      *
-     * @author Groupe 15
-     * retourne le rembourssement du soin calcule selon le contrat, le montant et le numero de soin.
+     * @author Groupe 15 retourne le rembourssement du soin calcule selon le
+     * contrat, le montant et le numero de soin.
      * @param contrat
      * @param montant
      * @param numSoin
      * @return le rembourcement
-    */
+     */
     public static Double appliquerLesContrat(String contrat, Double montant, int numSoin) {
-        
+
         Double rembourssement = 0.0;
         switch (contrat) {
 
             case "A":
-                if (numSoin == 0 ||numSoin == 100 || numSoin == 200 || numSoin == 500 ){
+                if (numSoin == 0 || numSoin == 100 || numSoin == 200 || numSoin == 500) {
                     rembourssement = montant * TAUX_25_POUR_CENT;
-                    
-                }else if((numSoin >= 300 && numSoin <= 399) || (numSoin == 400) 
-                        || (numSoin == 700) ){
+
+                } else if ((numSoin >= 300 && numSoin <= 399) || (numSoin == 400)
+                        || (numSoin == 700)) {
                     rembourssement = montant * TAUX_0_POUR_CENT;
-                    
-                }else{
+
+                } else {
                     rembourssement = montant * TAUX_40_POUR_CENT;
                 }
-                    
+
                 break;
 
             case "B":
-                
-                if (numSoin == 0 || numSoin == 500 ){
+
+                if (numSoin == 0 || numSoin == 500) {
                     rembourssement = montant * TAUX_50_POUR_CENT;
-                    
-                    if(rembourssement > 50){
-                        
+
+                    if (rembourssement > 50) {
+
                         rembourssement = 50.0;
                     }
-                    
-                }else if((numSoin >= 300 && numSoin <= 399)  ){
-                    
+
+                } else if ((numSoin >= 300 && numSoin <= 399)) {
+
                     rembourssement = montant * TAUX_50_POUR_CENT;
-                    
-                }else if(numSoin == 0){
+
+                } else if (numSoin == 0) {
                     rembourssement = montant * TAUX_50_POUR_CENT;
-                    
-                    if(rembourssement > 40){
-                        
+
+                    if (rembourssement > 40) {
+
                         rembourssement = 40.0;
                     }
-                }else if (numSoin == 200){
-                    
+                } else if (numSoin == 200) {
+
                     rembourssement = montant;
-                    
-                    if(rembourssement > 70){
-                        
+
+                    if (rembourssement > 70) {
+
                         rembourssement = 70.0;
                     }
-                    
-                }else if (numSoin == 400){
-                    
+
+                } else if (numSoin == 400) {
+
                     rembourssement = montant * TAUX_0_POUR_CENT;
-                    
-                }else if (numSoin == 600){
-                    
+
+                } else if (numSoin == 600) {
+
                     rembourssement = montant * TAUX_100_POUR_CENT;
-                    
-                }else {
-                    
+
+                } else {
+
                     rembourssement = montant * TAUX_70_POUR_CENT;
                 }
-                
+
                 break;
 
             case "C":
-                
+
                 rembourssement = montant * TAUX_90_POUR_CENT;
-                
+
                 break;
 
             case "D":
-                
-                if (numSoin == 200 || numSoin == 600 ){
-                    
+
+                if (numSoin == 200 || numSoin == 600) {
+
                     rembourssement = montant * TAUX_100_POUR_CENT;
-                    
-                    if(rembourssement > 100){
-                        
+
+                    if (rembourssement > 100) {
+
                         rembourssement = 100.0;
                     }
-                    
-                }else if((numSoin >= 300 && numSoin <= 399)  ){
-                    
-                    rembourssement = montant * TAUX_100_POUR_CENT ;
-                    
-                }else if(numSoin == 100 || numSoin == 500){
-                    
+
+                } else if ((numSoin >= 300 && numSoin <= 399)) {
+
                     rembourssement = montant * TAUX_100_POUR_CENT;
-                    
-                    if(rembourssement > 75){
-                        
+
+                } else if (numSoin == 100 || numSoin == 500) {
+
+                    rembourssement = montant * TAUX_100_POUR_CENT;
+
+                    if (rembourssement > 75) {
+
                         rembourssement = 75.0;
                     }
-                }else if (numSoin == 0){
-                    
+                } else if (numSoin == 0) {
+
                     rembourssement = montant * TAUX_100_POUR_CENT;
-                    
-                    if(rembourssement > 85){
-                        
+
+                    if (rembourssement > 85) {
+
                         rembourssement = 85.0;
                     }
-                    
-                }else if (numSoin == 400){
-                    
+
+                } else if (numSoin == 400) {
+
                     rembourssement = montant * TAUX_100_POUR_CENT;
-                    
-                    if(rembourssement > 65){
-                        
+
+                    if (rembourssement > 65) {
+
                         rembourssement = 65.0;
                     }
-                    
-                }else if (numSoin == 700){
-                    
+
+                } else if (numSoin == 700) {
+
                     rembourssement = montant * TAUX_100_POUR_CENT;
-                    
-                    if(rembourssement > 90){
-                        
+
+                    if (rembourssement > 90) {
+
                         rembourssement = 90.0;
                     }
-                    
+
                 }
                 break;
         }
 
         return rembourssement;
     }
+
     /**
      *
-     * @author Groupe 15
-     * retourne la liste des dates des soins.
+     * @author Groupe 15 retourne la liste des dates des soins.
      * @param numClient
      * @param contrat
      * @param mois
      * @param liste
      * @return
-    */
-    public static String creationFichierSortie(String numClient, String contrat, 
+     */
+    public static String creationFichierSortie(String numClient, String contrat,
             String mois, List<JSONObject> liste) {
 
         JSONObject objetJson = new JSONObject();
@@ -446,5 +436,5 @@ public class GestionAssurance {
 
         return objetJson.toString();
     }
-    
+
 }
